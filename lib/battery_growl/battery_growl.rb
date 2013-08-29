ea041b5662a8d2fc4ce32b750e802a92f7d1618d
youyo <1003ni2@gmail.com>
2013-08-29 14:27:05 +0900

    Add_method[num_check]

--- lib/battery_growl/battery_growl.rb
+++ lib/battery_growl/battery_growl.rb
@@ -6,6 +6,7 @@ class BatteryGrowl
   end
 
   def run(percent=10,host='localhost')
+    num_check(percent)
     battery_usage = check_battery
     subject = 'バッテリー容量低下'
     message = "残り#{battery_usage}%です。充電してください。"
@@ -16,6 +17,12 @@ class BatteryGrowl
   end
 
   private
+
+  def num_check(num)
+    raise 'Parameter is less than 1.' if num < 1
+    raise 'Parameter is greater than 100.' if num > 100
+    return true
+  end
 
   def check_battery
     cmd = "pmset -g batt|grep 'InternalBattery'|awk '{print $2}'|cut -d'%' -f1"
